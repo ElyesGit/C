e6bc3739fec7a4b3d3548ac3f537fea633eaf354
ElyesGit <47117886+ElyesGit@users.noreply.github.com>
2019-03-02 02:30:31 -0500

    lottery simulator

--- Lottery.cpp
+++ Lottery.cpp
@@ -1,5 +1,3 @@
-// Nom et Prenom : Lamouchi Elyes
-// Matricule : 20063403
 
 
 #include <stdio.h>         // 	On a inclus stdio.h afin de pouvoir utiliser prinft() dans la suite
@@ -11,11 +9,11 @@ int main()
  	int n1, n2, n3;  
  
  	srand(time(NULL));  
- 	n1 = rand()%10;        // 	Pour une valeur pseudo-aléatoire entre 0 et n
- 	n2 = rand()%10;       //   on doit doit affecter à notre variable rand()%(n+1)
+ 	n1 = rand()%10;        // 	Pour une valeur pseudo-alÃ©atoire entre 0 et n
+ 	n2 = rand()%10;       //   on doit doit affecter Ã  notre variable rand()%(n+1)
  	n3 = rand()%10;
  	
-	printf("Tirage de la quotidienne à 3 chiffres : %d %d %d ", n1, n2, n3);     // on a mis %d car on utilise des entiers
+	printf("Tirage de la quotidienne Ã  3 chiffres : %d %d %d ", n1, n2, n3);     // on a mis %d car on utilise des entiers
  	
 	return 0 ;
 }
